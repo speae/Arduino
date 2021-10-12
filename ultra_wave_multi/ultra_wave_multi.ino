77025efc05c76b54744eca532134c8a267e50df0
speae <speae123456@daum.net>
2021-10-12 11:41:10 +0900

    mkdir multi wave

--- ultra_wave_multi/ultra_wave_multi.ino
+++ ultra_wave_multi/ultra_wave_multi.ino
@@ -23,7 +23,7 @@ void setup() {
   pinMode(echoPin3, INPUT); // echoPin을 입력이다.
   pinMode(trigPin4, OUTPUT);
   pinMode(echoPin4, INPUT);
-  Serial.begin(9600); // 시리얼 포트를 시작합니다.
+  Serial.begin(115200); // 시리얼 포트를 시작합니다.
 }
 
 void loop() {
@@ -33,7 +33,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin1, LOW);
   duration1 = pulseIn(echoPin1, HIGH); // 트리거 핀에서 나온 펄스를 받아서 
-  distance1= duration1*0.034/2; // 거리를 측정합니다. 
+  distance1= (duration1/2)/29.1; // 거리를 측정합니다. 
 
   if (distance1 >= 500 || distance1 <= 0){ //500보다 크거나, 0보다 작으면 측정이 불가하다는 것을 프린트합니다.
     Serial.println("Out of range"); //측정 불가 라는 것을 프린트 합니다. 
@@ -43,7 +43,7 @@ void loop() {
     Serial.print ( distance1);// 거리 값
     Serial.println("cm"); // cm를 출력합니다.
   }
-  delay(2000); //2초마다 , 그리고 아래의 과정은 모두 동일합니다. 
+  delay(200); //2초마다 , 그리고 아래의 과정은 모두 동일합니다. 
 
   digitalWrite(trigPin2, LOW);
   delayMicroseconds(2);
@@ -51,7 +51,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin2, LOW);
   duration2 = pulseIn(echoPin2, HIGH);
-  distance2= duration2*0.034/2;
+  distance2= (duration2/2)/29.1;
 
   if (distance2 >= 500 || distance2 <= 0){
     Serial.println("Out of range");
@@ -61,7 +61,7 @@ void loop() {
     Serial.print(distance2);
     Serial.println("cm");
   }
-  delay(2000);
+  delay(200);
 
   digitalWrite(trigPin3, LOW);
   delayMicroseconds(2);
@@ -69,7 +69,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin3, LOW);
   duration3 = pulseIn(echoPin3, HIGH);
-  distance3= duration3*0.034/2;
+  distance3= (duration3/2)/29.1;
 
   if (distance3 >= 500 || distance3 <= 0){
     Serial.println("Out of range");
@@ -79,7 +79,7 @@ void loop() {
     Serial.print(distance3);
     Serial.println("cm");
   }
-  delay(2000);
+  delay(200);
 
   digitalWrite(trigPin4, LOW);
   delayMicroseconds(2);
@@ -87,7 +87,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin4, LOW);
   duration4 = pulseIn(echoPin4, HIGH);
-  distance4= duration4*0.034/2;
+  distance4= (duration4/2)/29.1;
 
   if (distance4 >= 500 || distance4 <= 0){
     Serial.println("Out of range");
@@ -98,7 +98,7 @@ void loop() {
     Serial.println("cm");
     Serial.println("");
   }
-  delay(2000);
+  delay(200);
 
 
 }
